869dd5190a9f826249d9b47d8b4008a2a612386d
Nathan Kamdem <kantnathan90@gmail.com>
2024-09-06 11:52:45 -0400

    initial commit

---
+++ lecon1_1/src/main/java/com/simplexarchitect/learn/springprofessional/lecon1_1/Lecon11Application.java
@@ -0,0 +1,12 @@
+package com.simplexarchitect.learn.springprofessional.lecon1_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lecon11Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Lecon11Application.class, args);
+	}
+
+}
---
+++ lecon1_2/src/main/java/com/simplexarchitect/learn/springprofessional/lecon1_2/Lecon12Application.java
@@ -0,0 +1,13 @@
+package com.simplexarchitect.learn.springprofessional.lecon1_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lecon12Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Lecon12Application.class, args);
+  }
+
+}
